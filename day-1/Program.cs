cb377f52abc416fd4900a55351cee4556bd1784a
JBillson <justin.billson@gmail.com>
2022-12-01 12:42:39 +0200

    refactored day-1

--- day-1/Program.cs
+++ day-1/Program.cs
@@ -1,8 +1,12 @@
 ﻿class Program{
     static void Main(string[] args){
+
+        // read input text
         var input = File.ReadAllLines("day-1/input.txt");
+        // create list to hold total calories
         List<int> totals = new List<int>();
-        int[] topCalorieCarriers = new int[3];
+
+        // calculate totals
         var currentCalorieCount = 0;
         foreach (var line in input)
         {
@@ -12,21 +16,26 @@
                 continue;
             }
 
-            var isValid = int.TryParse(line, out int number);
+            var isValid = int.TryParse(line, out int calories);
             if (!isValid) continue;
-            currentCalorieCount += number;
+            currentCalorieCount += calories;
         }
 
+        // order by descending and take top 3
         totals = totals.OrderByDescending(x => x).ToList();
         var top3 = totals.Take(3);
+
+        // log top 3 carriers
         Console.WriteLine("Top 3 Carriers:");
         foreach (var calories in top3)
             Console.WriteLine(calories);
 
+        // sum total in top 3
         var summedTotal = 0;
         foreach (var total in top3)
             summedTotal += total;
 
+        // log summed total
         Console.WriteLine($"Summed Total: {summedTotal}");
     }
 }
