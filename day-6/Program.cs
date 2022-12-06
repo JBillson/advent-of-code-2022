5ec5e5e664163d57b40f1dd25c550aa69ef4abb9
JBillson <justin.billson@gmail.com>
2022-12-06 15:47:33 +0200

    day 6

---
+++ day-6/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace day6;
+
+class Program{
+    private static string _input = string.Empty;
+    private static List<char> _charList = new();
+    private static int _distinctCharsRequired;
+
+    static void Main(string[] args){
+        _input = File.ReadAllText("input.txt");
+
+        // Part One
+        // _distinctCharsRequired = 4;
+        // Part Two
+        _distinctCharsRequired = 14;
+
+        var charCount = 0;
+        foreach (var x in _input)
+        {
+            charCount ++;
+            _charList.Add(x);
+
+            if (_charList.Count > _distinctCharsRequired){
+                _charList.RemoveAt(0);
+            }
+
+            if (_charList.Count == _charList.Distinct().Count() && _charList.Count == _distinctCharsRequired){
+                System.Console.WriteLine($"Marker found after {charCount} characters");
+                break;
+            }
+        }
+    }
+}
