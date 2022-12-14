a2d428bd1f67e9aab8cad7e76c1619c7ac48450d
JBillson <justin.billson@gmail.com>
2022-12-14 21:26:20 +0200

    day 11 part two.

--- day-11/Program.cs
+++ day-11/Program.cs
@@ -1,13 +1,15 @@
-﻿using System.Diagnostics;
+﻿using System.Numerics;
+using System.Diagnostics;
 namespace day11;
 
 class Program{
+    private static bool _divideWorryLevel;
     private const string newLine = "\r\n";
     private static string _input = string.Empty;
     private static List<Monkey> _monkeys = new();
-    private static int _round;
+    private static BigInteger _round;
     static void Main(string[] args){
-        _input = File.ReadAllText("input.txt");
+        _input = File.ReadAllText("test.txt");
         var instructions = _input.Split($"{newLine}{newLine}").ToList();
     
         // parse instructions
@@ -25,84 +27,100 @@ class Program{
             _monkeys.Add(monkey);
         }
 
-        StartGame(_monkeys);
+        var monkeyArray = _monkeys.ToArray();
+        
+        // // Part One
+        // _divideWorryLevel = true;
+        // StartGame(_monkeys, 20);
+
+        // Part Two
+        _divideWorryLevel = false;
+        StartGame(monkeyArray, 10000);
     }
 
-    private static void StartGame(List<Monkey> _monkeys){
+    private static void StartGame(Monkey[] monkeyArray, BigInteger roundsToRun){
         _round = 0;
-        while (_round < 20){
-            foreach (var monkey in _monkeys)
+        while (_round < roundsToRun){
+            foreach (var monkey in monkeyArray)
             {
-                foreach (var item in monkey.items.ToList())
-                {
+                foreach (var item in monkey.items.ToArray())
                     Inspect(monkey, item);
-                }
             }
 
             // increment round
             _round ++;
 
-            // log monkeys after round
-            System.Console.WriteLine($"After Round {_round}:");
-            foreach (var monkey in _monkeys)
-                System.Console.WriteLine($"Monkey {monkey.name}: {string.Join(',', monkey.items)}");
+            System.Console.WriteLine($"Round {_round}");
+           // log monkeys after round
+            if (_round % 1000 == 0 ||
+                _round == 1 ||
+                _round == 20)
+            {
+                System.Console.WriteLine($"After Round {_round}:");
+                foreach (var monkey in monkeyArray)
+                    System.Console.WriteLine($"Monkey {monkey.name} inspected items {monkey.inspectionCount} times.");
+
+            }
+            // if (_round < 20){
+            //     System.Console.WriteLine($"----- Round {_round} -----");
+            //     foreach (var monkey in _monkeys)
+            //         System.Console.WriteLine($"Monkey {monkey.name}: {string.Join(',', monkey.items)}");
+            // }
         }
 
         // get two most active monkeys
-        var monkeys = _monkeys.OrderByDescending(x => x.inspectionCount);
+        var monkeys = monkeyArray.OrderByDescending(x => x.inspectionCount);
         var activeMonkeys = monkeys.Take(2);
 
-        foreach (var monkey in monkeys)
-            System.Console.WriteLine($"Monkey {monkey.name} inspected items {monkey.inspectionCount} times.");
-
         // calculate monkey business
-        var monkeyBusiness = 1;
+        BigInteger monkeyBusiness = 1;
         foreach (var monkey in activeMonkeys)
             monkeyBusiness *= monkey.inspectionCount;
         
         System.Console.WriteLine($"Monkey Business: {monkeyBusiness}");
     }
 
-    private static void Inspect(Monkey monkey, int itemWorryLevel){
+    private static void Inspect(Monkey monkey, BigInteger itemWorryLevel){
         // increment monkey inspect count
         monkey.inspectionCount ++;
 
         // parse operation
         var amountString = monkey.operation.Split(" ")[^1];
         var symbol = monkey.operation.Split(" ")[^2];
-        var amountInt = 0;
+        BigInteger amountBigInteger = 0;
         if (amountString.Contains("old")){
-            amountInt = itemWorryLevel;
+            amountBigInteger = itemWorryLevel;
         }
         else{
-            amountInt = int.Parse(amountString);
+            amountBigInteger = BigInteger.Parse(amountString);
         }
         
         // inspect item & calculate new itemWorryLevel
         switch (symbol)
         {
             case "*":
-                itemWorryLevel *= amountInt;
+                itemWorryLevel *= itemWorryLevel;
             break;
             case "+":
-                itemWorryLevel += amountInt;
+                itemWorryLevel += itemWorryLevel;
             break;
             case "-":
-                itemWorryLevel -= amountInt;
+                itemWorryLevel -= itemWorryLevel;
             break;
             case "/":
-                itemWorryLevel /= amountInt;
+                itemWorryLevel /= itemWorryLevel;
             break;
             default:
                 throw new Exception($"Unknown Operator {symbol}");
         }
 
         // divide worryLevel by 3
-        itemWorryLevel /= 3;
+        if (_divideWorryLevel)
+            itemWorryLevel = itemWorryLevel / 3;
 
         // run test
         var test = monkey.test;
-        var divisibleBy = int.Parse(test.testCase.Split(" ")[^1]);
+        BigInteger divisibleBy = BigInteger.Parse(test.testCase.Split(" ")[^1]);
         var monkeyName = string.Empty;
         if (itemWorryLevel % divisibleBy == 0){
             monkeyName = test.ifTrue.Split(" ")[^1]; 
@@ -119,10 +137,10 @@ class Program{
 
 class Monkey{
     public string name;
-    public List<int> items = new();
+    public List<BigInteger> items = new();
     public string operation = string.Empty;
     public Test test;
-    public int inspectionCount;
+    public BigInteger inspectionCount;
     public Monkey(string name, string startingItems, string operation, string testCase, string ifTrue, string ifFalse){
         // name
         this.name = name.Split(":")[0].Split(" ")[1];
@@ -130,7 +148,7 @@ class Monkey{
         // items
         var itemList = startingItems.Split(": ")[1].Split(",");
         foreach (var item in itemList)
-            items.Add(int.Parse(item));
+            items.Add(BigInteger.Parse(item));
 
         // operation
         this.operation = operation.Split(": ")[1];
@@ -140,12 +158,8 @@ class Monkey{
         ifTrue = ifTrue.Split(": ")[1];
         ifFalse = ifFalse.Split(": ")[1];
         test = new Test(testCase, ifTrue, ifFalse);
-        System.Console.WriteLine($"Creating Monkey:\nName: {this.name}\nStarting Items: {string.Join(',', this.items)}\nOperation: {this.operation}\nTest: {testCase}\n\tIf true: {ifTrue}\n\tIf false: {ifFalse}");
+        // System.Console.WriteLine($"Creating Monkey:\nName: {this.name}\nStarting Items: {string.Join(',', this.items)}\nOperation: {this.operation}\nTest: {testCase}\n\tIf true: {ifTrue}\n\tIf false: {ifFalse}");
     }
-
-    // public void Log(this Monkey m){
-    //     System.Console.WriteLine($"Creating Monkey:\nName: {m.name}\nStarting Items: {string.Join(',', m.items)}\nOperation: {m.operation}\nTest: {m.test.testCase}\n\tIf true: {m.test.ifTrue}\n\tIf false: {m.test.ifFalse}");
-    // }
 }
 
 class Test{
